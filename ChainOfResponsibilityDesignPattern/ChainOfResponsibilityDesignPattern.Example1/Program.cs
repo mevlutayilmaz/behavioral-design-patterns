477382ba495ffae18084688b7be8dbfd222a5d53
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2025-02-08 23:51:47 +0300

    chain of responsibility design pattern example

---
+++ ChainOfResponsibilityDesignPattern/ChainOfResponsibilityDesignPattern.Example1/Program.cs
@@ -0,0 +1,109 @@
+﻿
+MonkeyHandler monkey = new();
+SquirrelHandler squirrel = new();
+DogHandler dog = new();
+
+monkey.SetNext(squirrel).SetNext(dog);
+
+Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+Client.ClientCode(monkey);
+Console.WriteLine();
+
+Console.WriteLine("Subchain: Squirrel > Dog\n");
+Client.ClientCode(squirrel);
+
+public interface IHandler
+{
+    IHandler SetNext(IHandler handler);
+    object Handle(object request);
+}
+
+public abstract class AbstractHandler : IHandler
+{
+    private IHandler _nextHandler;
+
+    public IHandler SetNext(IHandler handler)
+    {
+        _nextHandler = handler;
+        return handler;
+    }
+
+    public virtual object Handle(object request)
+    {
+        if (_nextHandler != null)
+        {
+            return _nextHandler.Handle(request);
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+
+public class MonkeyHandler : AbstractHandler
+{
+    public override object Handle(object request)
+    {
+        if ((request as string) == "Banana")
+        {
+            return $"Monkey: I'll eat the {request.ToString()}.\n";
+        }
+        else
+        {
+            return base.Handle(request);
+        }
+    }
+}
+
+public class SquirrelHandler : AbstractHandler
+{
+    public override object Handle(object request)
+    {
+        if (request.ToString() == "Nut")
+        {
+            return $"Squirrel: I'll eat the {request.ToString()}.\n";
+        }
+        else
+        {
+            return base.Handle(request);
+        }
+    }
+}
+
+public class DogHandler : AbstractHandler
+{
+    public override object Handle(object request)
+    {
+        if (request.ToString() == "MeatBall")
+        {
+            return $"Dog: I'll eat the {request.ToString()}.\n";
+        }
+        else
+        {
+            return base.Handle(request);
+        }
+    }
+}
+
+public class Client
+{
+    public static void ClientCode(AbstractHandler handler)
+    {
+        foreach (var food in new List<string> { "Nut", "Banana", "Cup of coffee" })
+        {
+            Console.WriteLine($"Client: Who wants a {food}?");
+
+            var result = handler.Handle(food);
+
+            if (result != null)
+            {
+                Console.Write($"   {result}");
+            }
+            else
+            {
+                Console.WriteLine($"   {food} was left untouched.");
+            }
+        }
+    }
+}
